e63f29850f0f1d6389830f5e3e1a29310eecc76c
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-06-30 04:21:44 +0300

    Update usage.py

--- app/cli/usage.py
+++ app/cli/usage.py
@@ -2,12 +2,11 @@ USAGE = """
 Smart Event
 
 Usage:
-  main.py [--city=<city>] [--country=<country>] --topics=<topics> [--key=<key>]
+  main.py [--city=<city>] --topics=<topics> [--key=<key>]
   main.py -h | --help
 
 Options:
   --city=<city>        Optional city name [default: cairo]. You can specify multiple cities separated by commas.
-  --country=<country>  Optional country name (only one country allowed).
   --topics=<topics>      Required topics name (e.g., software, market, etc.).
   --key=<key>          Optional Google API key for Gemini (GEMINI) AI access.
   -h --help            Show this help message.
@@ -16,12 +15,12 @@ Description:
   ┌───────────────────────────────────────────────────────────────┐
   │ 💬 This project utilizes ✨ Gemini AI ✨ to intelligently     │
   │ retrieve, summarize, and structure event information based    │
-  │ on specific cities or countries and a given topics.           │
+  │ on specific cities and a given topics.                        │
   │ It gathers event names, descriptions, locations, and links —  │
   │ then generates an Excel file to organize the collected data.  │
   │                                                               │
   │ 🔧 Example command:                                           │
-  │     main.py --city=cairo --country=egypt                      │
+  │     main.py --city=cairo                                      │
   │             --topics=software --key=your-google-api-key       │
   │                                                               │
   │ 🔑 How to Get a Google API Key:                               │
