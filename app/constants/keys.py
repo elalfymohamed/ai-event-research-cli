ffbb177b267d9f97c6662ec1357562271277239a
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-06-30 23:48:58 +0300

    Update keys.py

--- app/constants/keys.py
+++ app/constants/keys.py
@@ -1,5 +1,5 @@
-# The filename used to store the Google API key locally
-FILE_KEY_NAME = "google_key"
+# The filename used to store the API key locally
+FILE_KEY_NAME = "key"
 
 # The environment variable name used to load the key
-VARIABLE_KEY_ENV = "GOOGLE_API_KEY"
+VARIABLE_KEY_ENV = "FIRECRAW_API_KEY"
