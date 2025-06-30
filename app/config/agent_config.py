bd671adcc93a3998d54486d90abf882f82aa17e4
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-06-30 04:19:15 +0300

    Update agent_config.py

--- app/config/agent_config.py
+++ app/config/agent_config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 #
 @dataclass
 class AgentConfig:
-    model: str = "gemini-2.5-flash"  # gemini-2.5-flash  |  gemini-2.5-pro | gemini-2.5-flash-preview-04-17
-    temperature: float =0.2
+    model: str = "gemini-2.5-pro"  # gemini-2.5-flash  |  gemini-2.5-pro | gemini-2.5-flash-preview-04-17
+    temperature: float =0.4
     top_p: float =1.0
     top_k: int =1
     max_output_tokens: int | None = None
