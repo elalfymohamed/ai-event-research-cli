ed564737cbb22d0aefe7d1c1e0d29df8b9d79358
Elalfy Mohamed <elalfy.mohamed19@gmail.com>
2025-07-04 19:25:16 +0300

    fix: correct months valdation logic in validators.py

--- app/cli/validators.py
+++ app/cli/validators.py
@@ -1,9 +1,8 @@
 # Local imports
 from utilities import is_valid_firecrawl_key_format
 
-class ValidationError(Exception):
+class ValidationError(ValueError):
     pass
-
 
 def validate_required_args(args: dict) -> None:
     """
@@ -24,14 +23,16 @@ def validate_required_args(args: dict) -> None:
         raise ValidationError("❌  --city must be comma-separated values with no spaces.")
     
     # Validate --months
-    try:
-        months = int(months)
-    except (TypeError, ValueError):
-        raise ValidationError("❌ --months must be an integer between 1 and 4.")
-    
-    if int(months) < 1 or int(months) > 4:
-        raise ValidationError("❌ --months must be between 1 and 4 for get best .")
+    if months:
+        try:
+            months = int(months)
+        except (TypeError, ValueError):
+            raise ValidationError("❌ --months must be an integer between 1 and 4.")
+        
+        if int(months) < 1 or int(months) > 4:
+            raise ValidationError("❌ --months must be between 1 and 4 for get best .")
 
     # Validate --key
     if key and not is_valid_firecrawl_key_format(key):
         raise ValidationError("❌ Invalid Firecrawl API key format.")
+
