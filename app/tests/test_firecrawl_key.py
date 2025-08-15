53c9b0d239f9c92db2e14082dce94698a80fce3f
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-08-15 03:33:36 +0300

    Update test_firecrawl_key.py

--- app/tests/test_firecrawl_key.py
+++ app/tests/test_firecrawl_key.py
@@ -1,27 +1,34 @@
 import pytest
-
 from utilities.valid_firecrawl_key_format import is_valid_firecrawl_key_format
 
 
-def test_valid_key_format():
-  assert is_valid_firecrawl_key_format("fc-1234567890abcdef1234567890abcdef")
-  assert  is_valid_firecrawl_key_format("fc-1234567890abcdef1234567890abcdefABCD")
-  assert  is_valid_firecrawl_key_format("fc-1234567890abcdef_1234567890abcdef-AB")
+@pytest.mark.parametrize("key", [
+    "fc-1234567890abcdef1234567890abcdef",
+    "fc-1234567890abcdef1234567890abcdefABCD",
+    "fc-1234567890abcdef_1234567890abcdef-AB"
+])
+def test_valid_key_format(key):
+    assert is_valid_firecrawl_key_format(key)
 
 
-def test_invalid_key_format():
-    assert not  is_valid_firecrawl_key_format("fc-1234")
-    assert not  is_valid_firecrawl_key_format("")
-    assert not  is_valid_firecrawl_key_format(1234)
-    assert not  is_valid_firecrawl_key_format(None)
-    assert not  is_valid_firecrawl_key_format("fz-1234567890abcdef1234567890abcdef")
-    assert not  is_valid_firecrawl_key_format("fc-1234567890abcdef 1234567890abcdef")
-    assert not  is_valid_firecrawl_key_format("fc-1234567890abcdef!@#$%^&*()")
+@pytest.mark.parametrize("key", [
+    "fc-1234",
+    "",
+    1234,
+    None,
+    "fz-1234567890abcdef1234567890abcdef",
+    "fc-1234567890abcdef 1234567890abcdef",
+    "fc-1234567890abcdef!@#$%^&*()"
+])
+def test_invalid_key_format(key):
+    assert not is_valid_firecrawl_key_format(key)
 
 
-def test_edge_key_format():
-    assert  is_valid_firecrawl_key_format("fc-" + "a"*32)
-    assert  is_valid_firecrawl_key_format("fc-" + "a"*40)
-    assert  is_valid_firecrawl_key_format("fc-" + "a"*100)
-    assert not is_valid_firecrawl_key_format("fc-" + "A"*31)
-   
+@pytest.mark.parametrize("key, expected", [
+    ("fc-" + "a"*32, True),
+    ("fc-" + "a"*40, True),
+    ("fc-" + "a"*100, True),
+    ("fc-" + "A"*31, False)
+])
+def test_edge_key_format(key, expected):
+    assert is_valid_firecrawl_key_format(key) == expected
